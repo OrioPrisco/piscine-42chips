abd8590f4b744e1b4f446a073acf63e0b6bc0bee
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2023-03-27 16:32:13 +0200

    day04 : remove makefiles and test files

--- day03/ex00/main.c
+++ day03/ex00/main.c
@@ -1,27 +1,6 @@
 #include <avr/io.h>
 #include <util/delay.h>
-
-#define BAUD 115200
-#define MYUBRR ((F_CPU/8/BAUD-1)/2)
-
-
-void uart_init()
-{
-	//asynchronous normal mode
-	//U2Xn is 0 by default
-	//UMSELn too
-	UBRR0 = MYUBRR;	//baud rate
-	UCSR0B = (1 << TXEN0);//enable tx
-	UCSR0C = (0<<USBS0)|(1<<UCSZ01)|(1<<UCSZ00); // frame format, 8 data 1 stop
-
-}
-
-void uart_tx(unsigned char c)
-{
-	while (!(UCSR0A & (1<<UDRE0)))
-		;
-	UDR0 = c;
-}
+#include "uart.h"
 
 int main()
 {

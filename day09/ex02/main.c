a364284d23b5d63419f284b5e5dd38b3b68ef2fa
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2023-03-27 15:52:58 +0200

    day09 ex02 : update call to _7seg_display_one

--- day09/ex02/main.c
+++ day09/ex02/main.c
@@ -6,14 +6,12 @@
 #include "gpio.h"
 #include "pca9555.h"
 
-
-_Bool previous_state = 0;
 int main()
 {
 	uart_init();
 	i2c_init();
 	i2c_expander_init();
-	_7seg_display_one(I2C_EXPANDER_CA4, ~(1 << I2C_EXPANDER_7SEG_f | 1 << I2C_EXPANDER_7SEG_c));
+	_7seg_display_one(I2C_EXPANDER_CA4, ~(1 << I2C_EXPANDER_7SEG_f | 1 << I2C_EXPANDER_7SEG_c), _7seg_keep_mask());
 	while (1)
 	{
 	}
